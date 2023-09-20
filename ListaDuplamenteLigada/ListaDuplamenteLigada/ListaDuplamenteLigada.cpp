4dbf5eed0bf595624424457ba2d4d801cc3f86e7
Maurício de Carvalho <mauriciocr223@gmail.com>
2023-09-20 14:52:21 -0300

    Criação das funções requeridas

--- ListaDuplamenteLigada/ListaDuplamenteLigada/ListaDuplamenteLigada.cpp
+++ ListaDuplamenteLigada/ListaDuplamenteLigada/ListaDuplamenteLigada.cpp
@@ -150,19 +150,63 @@ void inserirElemento()
 // funções a serem implementadas no exericio
 void exibirReverso()
 {
-
+	if (primeiro == NULL) {
+		cout << "Lista vazia \n";
+		return;
+	}
+	else {
+		cout << "Elementos: \n";
+		NO* aux = ultimo;
+		while (aux != NULL) {
+			cout << aux->valor << endl;
+			aux = aux->ant;
+		}
+	}
 }
 
 void excluirPrimeiroElemento()
 {
+	if (primeiro == NULL) {
+		cout << "Lista vazia \n";
+		return;
+	}
 
+	NO* aux = primeiro;
+	primeiro = primeiro->prox;
+	if (primeiro != NULL)
+	{
+		primeiro->ant = NULL;
+	}
+	else
+	{
+		ultimo = NULL;
+	}
+
+	free(aux);
+	cout << "O Elemento foi excluído. \n";	
 }
 
 void excluirUltimoElemento()
 {
+	if (ultimo == NULL) {
+		cout << "Lista vazia \n";
+		return;
+	}
+	
+	NO* ult = ultimo;
+	ultimo = ultimo->ant;
+	if (ultimo != NULL)
+	{
+		ultimo->prox = NULL;
+	}
+	else
+	{
+		primeiro = NULL;
+	}
 
+	free(ult);
+	cout << "O Elemento foi excluído. \n";
 }
-
 
 
 
